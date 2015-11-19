c6735f3b3672282f3723d632b1fd5952ffc88d7a
Mohammed Rizvi <morizvi07@gmail.com>
2015-11-19 10:12:54 -0500

    Added function that will delete added row

--- js/addrow.js
+++ js/addrow.js
@@ -58,5 +58,18 @@ function insertRow() {
   cell2.innerHTML = inputs[1].value;
   cell3.innerHTML = inputs[2].value;
   cell4.innerHTML = '<button type="button" class="btn btn-danger deleteButton">X</button>';
+
+
+//Make delete button remove added row 
+  var deleteButton = document.getElementsByClassName('deleteButton')
+
+  for (var i = 0; i < deleteButton.length;  i++) {
+    deleteButton[i].addEventListener("click", deleteRecord)
+  }
+
+  function deleteRecord() {
+    this.parentElement.parentElement.remove();
+  }
+
 }
 
