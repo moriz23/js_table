75d7452d6a049badd9850beed053cdb02dc3e1c7
Mohammed Rizvi <morizvi07@gmail.com>
2015-11-19 08:02:54 -0500

    Added vode so that a delete button would show after adding a row

--- js/addrow.js
+++ js/addrow.js
@@ -47,17 +47,16 @@ function insertRow() {
   var rowInsert = table.insertRow(table.rows.length-1);
   var inputs = document.getElementsByClassName("new-input");
 
-  //Insert new table cells (td) elements at the first, second, and third position of the (tr) element
+   //Insert new table cells (td) elements at the first, second, and third position of the (tr) element
   var cell1 = rowInsert.insertCell(0); 
   var cell2 = rowInsert.insertCell(1);
   var cell3 = rowInsert.insertCell(2);
-  var cell1Val = inputs[0].value;
-  var cell2Val = inputs[1].value;
-  var cell3Val = inputs[2].value;
+  var cell4 = rowInsert.insertCell(3);
 
   //Text will be added to new row cells:
   cell1.innerHTML = inputs[0].value;
   cell2.innerHTML = inputs[1].value;
   cell3.innerHTML = inputs[2].value;
+  cell4.innerHTML = '<button type="button" class="btn btn-danger deleteButton">X</button>';
 }
 
